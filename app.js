f9e6877fda6bbfcb218dd56bf20df85a26c8320f
LinZH-1995 <lin84610@gmail.com>
2023-03-22 13:59:41 +0800

    feat: express setting init

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const methodOverrirde = require('method-override')
+
+const app = express()
+const port = 3000
+
+app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+app.use(methodOverrirde('_method'))
+app.use(express.urlencoded({ extended: true }))
+
+app.listen(port, () => {
+  console.log(`Now Server is working on localhost:${port}`)
+})
