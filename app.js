930f4b3495e67c786b67ceb34751af661510570f
LinZH-1995 <lin84610@gmail.com>
2023-03-22 17:37:19 +0800

    feat: set index router and home route

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverrirde = require('method-override')
 
+const routes = require('./routes')
 require('./config/mongoose.js')
 
 const app = express()
@@ -12,6 +13,8 @@ app.set('view engine', 'hbs')
 
 app.use(methodOverrirde('_method'))
 app.use(express.urlencoded({ extended: true }))
+
+app.use(routes)
 
 app.listen(port, () => {
   console.log(`Now Server is working on localhost:${port}`)
