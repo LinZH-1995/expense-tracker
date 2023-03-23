b5ded9e5f985b3bee5cbf99344cd2accc3f9bdd3
LinZH-1995 <lin84610@gmail.com>
2023-03-23 17:53:13 +0800

    feat: add category filter route
    Squashed commits:
    
    [7622b69] feat: add category filter route

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -23,4 +23,31 @@ router.get('/', (req, res) => {
            .catch(error => console.log(error))
 })
 
+router.get('/search', (req, res) => {
+  const key = req.query.categorySelect
+  console.log(key)
+
+  if (key === '--') {
+    return res.redirect('/')
+  }
+
+  return Record.find({ category: key}).lean()
+           .then(records => {
+              Category.find().lean()
+                .then(categorys => {
+                  let totalAmount = 0
+                  records.forEach(record => {
+                    const { icon } = categorys.find(category => category.name === record.category) || '--'
+                    record.icon = icon
+                    record.date = record.date.toISOString().slice(0, 10)
+                    totalAmount += record.amount
+                  })
+                  return totalAmount.toLocaleString()
+                })
+                .then(totalAmount => res.render('index', { records, totalAmount, key }))
+                .catch(error => console.log(error))
+           })
+           .catch(error => console.log(error))
+})
+
 module.exports = router
