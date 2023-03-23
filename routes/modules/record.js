7793d71939b53990a309e0ddf7c89ffe87a12100
LinZH-1995 <lin84610@gmail.com>
2023-03-23 17:20:21 +0800

    feat: add edit page and route
    feat: add edit page and route

--- routes/modules/record.js
+++ routes/modules/record.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const Record = require('../../models/record.js')
 const Category = require('../../models/category.js')
+const record = require('../../models/record.js')
 
 router.get('/new', (req, res) => {
   return res.render('new')
@@ -11,6 +12,31 @@ router.post('/', (req, res) => {
 return Record.create(req.body)
          .then(() => res.redirect('/'))
          .catch(error => console.log(error))
+})
+
+router.get('/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Record.findById(id).lean()
+           .then(record => {
+             record.date = record.date.toISOString().slice(0, 10)
+             return record
+           })
+           .then(record => res.render('edit', { record }))
+           .catch(error => console.log(error))
+})
+
+router.put('/:id', (req, res) => {
+  console.log(req.body)
+  // { name: '早餐', date: '2023-03-21', category: 'food', amount: '100' }
+  const id = req.params.id
+  const editData = req.body
+  return Record.findById(id)
+           .then(record => {
+            record = Object.assign(record, editData)
+            return record.save()
+           })
+           .then(() => res.redirect('/'))
+           .catch(error => console.log(error))
 })
 
 module.exports = router
