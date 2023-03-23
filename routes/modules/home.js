1c27087bd8c41b08217e90629c7b564eda794631
LinZH-1995 <lin84610@gmail.com>
2023-03-23 15:19:20 +0800

    feat: set home page route

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -4,7 +4,23 @@ const Record = require('../../models/record.js')
 const Category = require('../../models/category.js')
 
 router.get('/', (req, res) => {
-  res.render('index')
+  return Record.find().lean()
+           .then(records => {
+              Category.find().lean()
+                .then(categorys => {
+                  let totalAmount = 0
+                  records.forEach(record => {
+                    const { icon } = categorys.find(category => category.name === record.category)
+                    record.icon = icon
+                    record.date = record.date.toISOString().slice(0, 10)
+                    totalAmount += record.amount
+                  })
+                  return totalAmount.toLocaleString()
+                })
+                .then(totalAmount => res.render('index', { records, totalAmount }))
+                .catch(error => console.log(error))
+            })
+           .catch(error => console.log(error))
 })
 
 module.exports = router
