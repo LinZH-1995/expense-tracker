7ae54ed74378f75cc70b83e7536427c3a0537041
LinZH-1995 <lin84610@gmail.com>
2023-04-01 22:35:09 +0800

    feat: add register feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,5 +1,7 @@
+const bcrypt = require('bcryptjs')
 const express = require('express')
 const router = express.Router()
+const User = require('../../models/user.js')
 
 router.get('/login', (req, res) => {
   res.render('login')
@@ -13,7 +15,25 @@ router.get('/register', (req, res) => {
 })
 
 router.post('/register', (req, res) => {
-  res.render('register')
+  const { name, email, password, confirmPassword } = req.body
+
+  if (password !== confirmPassword) {
+    const error = 'Password 與 Confirm Password 必須一致！'
+    return res.render('register', { name, email, password, confirmPassword, error })
+  }
+
+  User.findOne({ email })
+    .then(user => {
+      if (user) {
+        const error = 'Email 已經註冊過了。'
+        return res.render('register', { name, email, password, confirmPassword, error })
+      }
+      return bcrypt.genSalt(10)
+    })
+    .then(salt => bcrypt.hash(password, salt))
+    .then(hash => User.create({ name, email, password: hash }))
+    .then(() => res.redirect('/users/login'))
+    .catch(err => console.log(err))
 })
 
 module.exports = router
