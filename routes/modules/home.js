f1758327be689b31e483b8c3a67a424b65588656
LinZH-1995 <lin84610@gmail.com>
2023-04-02 20:50:34 +0800

    feat: add search condition of month

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const Record = require('../../models/record.js')
 const Category = require('../../models/category.js')
+const record = require('../../models/record.js')
 
 router.get('/', (req, res) => {
   const userId = req.user._id
@@ -26,14 +27,57 @@ router.get('/', (req, res) => {
 
 router.get('/search', (req, res) => {
   const userId = req.user._id
-  const key = req.query.categorySelect
+  const { categorySelect, monthSelect } = req.query
 
-  if (key === '--') {
+  if (categorySelect === '--' && monthSelect === '--') {
     return res.redirect('/')
   }
 
-  Record.find({ userId, category: key }).lean()
+  if (categorySelect !== '--' && monthSelect !== '--') {
+    return Record.find({ userId, category: categorySelect }).lean()
+      .then(records => {
+        records = records.filter(record => record.date.getMonth() === Number(monthSelect) - 1)
+        Category.find().lean()
+          .then(categorys => {
+            let totalAmount = 0
+            records.forEach(record => {
+              record.icon = categorys.find(category => category.name === record.category).icon || '--'
+              record.date = record.date.toISOString().slice(0, 10)
+              totalAmount += record.amount
+            })
+            return totalAmount.toLocaleString()
+          })
+          .then(totalAmount => res.render('index', { records, totalAmount, categorySelect, monthSelect }))
+          .catch(error => console.log(error))
+      })
+      .catch(error => console.log(error))
+  }
+
+  const key = categorySelect !== '--' ? 'category' : 'date'
+  const value = key === 'category' ? categorySelect : monthSelect
+  if (key === 'category') {
+    return Record.find({ userId }).lean()
+      .then(records => {
+        records = records.filter(record => record[key] === value)
+        Category.find().lean()
+          .then(categorys => {
+            let totalAmount = 0
+            records.forEach(record => {
+              record.icon = categorys.find(category => category.name === record.category).icon || '--'
+              record.date = record.date.toISOString().slice(0, 10)
+              totalAmount += record.amount
+            })
+            return totalAmount.toLocaleString()
+          })
+          .then(totalAmount => res.render('index', { records, totalAmount, categorySelect }))
+          .catch(error => console.log(error))
+      })
+      .catch(error => console.log(error))
+  }
+
+  Record.find({ userId }).lean()
     .then(records => {
+      records = records.filter(record => record[key].getMonth() === Number(value) - 1)
       Category.find().lean()
         .then(categorys => {
           let totalAmount = 0
@@ -44,10 +88,11 @@ router.get('/search', (req, res) => {
           })
           return totalAmount.toLocaleString()
         })
-        .then(totalAmount => res.render('index', { records, totalAmount, key }))
+        .then(totalAmount => res.render('index', { records, totalAmount, monthSelect }))
         .catch(error => console.log(error))
     })
     .catch(error => console.log(error))
+
 })
 
 module.exports = router
